37447c73ced9a5dec4f27bc26a38c9b0ed2f8f9b
Raco <130675534+Kameil@users.noreply.github.com>
2026-03-20 21:57:39 -0300

    8 gen 1

--- packages/python-torch/build.sh
+++ packages/python-torch/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="Tensors and Dynamic neural networks in Python"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.10.0"
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=git+https://github.com/pytorch/pytorch
 TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
 TERMUX_PKG_DEPENDS="abseil-cpp, libc++, libopenblas, libprotobuf, python, python-numpy, python-pip"
@@ -11,10 +11,7 @@ TERMUX_PKG_BUILD_DEPENDS="vulkan-headers, vulkan-loader-android"
 TERMUX_PKG_HOSTBUILD=true
 TERMUX_PKG_PYTHON_COMMON_BUILD_DEPS="wheel, pyyaml, typing_extensions"
 TERMUX_PKG_PYTHON_CROSS_BUILD_DEPS="numpy"
-# USE_DISTRIBUTED=ON fixes
-# ModuleNotFoundError: No module named 'torch._C._distributed_c10d'; 'torch._C' is not a package
-# in
-# python -c "from torch.distributed._tensor import DTensor"
+
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 -DANDROID_NO_TERMUX=OFF
@@ -36,13 +33,16 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DUSE_MAGMA=OFF
 -DUSE_NCCL=OFF
 -DUSE_NNPACK=ON
--DCXX_AVX512_FOUND=OFF
--DCXX_AVX2_FOUND=OFF
+-DUSE_XNNPACK=ON
+-DUSE_PYTORCH_QNNPACK=ON
 -DUSE_VULKAN=ON
 -DUSE_DISTRIBUTED=ON
+-DUSE_OPENMP=ON
+-DBLAS=OpenBLAS
+-DUSE_EIGEN_FOR_BLAS=OFF
+-DUSE_MKLDNN=OFF
 -DANDROID_NDK=${NDK}
 -DANDROID_NDK_HOST_SYSTEM_NAME=linux-$HOSTTYPE
--DCMAKE_POLICY_VERSION_MINIMUM=3.5
 "
 
 TERMUX_PKG_RM_AFTER_INSTALL="
@@ -53,39 +53,39 @@ include/fmt
 "
 
 termux_step_host_build() {
-	termux_setup_cmake
-	cmake "$TERMUX_PKG_SRCDIR/third_party/sleef"
-	make -j "$TERMUX_PKG_MAKE_PROCESSES" mkrename mkrename_gnuabi mkmasked_gnuabi mkalias mkdisp
+    termux_setup_cmake
+    cmake "$TERMUX_PKG_SRCDIR/third_party/sleef"
+    make -j "$TERMUX_PKG_MAKE_PROCESSES" mkrename mkrename_gnuabi mkmasked_gnuabi mkalias mkdisp
 }
 
 termux_step_pre_configure() {
-	LDFLAGS+=" -fopenmp -static-openmp"
+    # --- OTIMIZAÇÕES DE HARDWARE (O PONTO CHAVE) ---
+    # Ativa instruções específicas para ARMv8.6-A (BF16, I8MM, DotProd)
+    # Trocamos -Oz por -O3 para máxima performance computacional
+    export CFLAGS=" -march=armv8.6-a+fp16+dotprod+i8mm+bf16 -O3"
+    export CXXFLAGS=" -march=armv8.6-a+fp16+dotprod+i8mm+bf16 -O3"
+    LDFLAGS+=" -fopenmp -static-openmp"
 
-	export PYTHONPATH="${PYTHONPATH}:${TERMUX_PKG_SRCDIR}"
-	find "$TERMUX_PKG_SRCDIR" -name CMakeLists.txt -o -name '*.cmake' ! -name 'VulkanCodegen*' |
-		xargs -n 1 sed -i \
-			-e 's/\([^A-Za-z0-9_]ANDROID\)\([^A-Za-z0-9_]\)/\1_NO_TERMUX\2/g' \
-			-e 's/\([^A-Za-z0-9_]ANDROID\)$/\1_NO_TERMUX/g'
+    export PYTHONPATH="${PYTHONPATH}:${TERMUX_PKG_SRCDIR}"
+    find "$TERMUX_PKG_SRCDIR" -name CMakeLists.txt -o -name '*.cmake' ! -name 'VulkanCodegen*' |
+        xargs -n 1 sed -i \
+            -e 's/\([^A-Za-z0-9_]ANDROID\)\([^A-Za-z0-9_]\)/\1_NO_TERMUX\2/g' \
+            -e 's/\([^A-Za-z0-9_]ANDROID\)$/\1_NO_TERMUX/g'
 
-	termux_setup_protobuf
+    termux_setup_protobuf
 
-	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="
-	-DPython_EXECUTABLE=$(command -v python3)
-	-DPROTOBUF_PROTOC_EXECUTABLE=$(command -v protoc)
-	-DCAFFE2_CUSTOM_PROTOC_EXECUTABLE=$(command -v protoc)
-	"
+    TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="
+    -DPython_EXECUTABLE=$(command -v python3)
+    -DPROTOBUF_PROTOC_EXECUTABLE=$(command -v protoc)
+    -DCAFFE2_CUSTOM_PROTOC_EXECUTABLE=$(command -v protoc)
+    "
 
-	# /home/builder/.termux-build/python-torch/src/torch/csrc/jit/codegen/onednn/graph_helper.h:3:10: fatal error: 'oneapi/dnnl/dnnl_graph.hpp' file not found
-	TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="
-	-DUSE_MKLDNN=OFF
-	"
-
-	ln -sf "$TERMUX_PKG_BUILDDIR" build
+    ln -sf "$TERMUX_PKG_BUILDDIR" build
 }
 
 termux_step_make_install() {
-	export PYTORCH_BUILD_VERSION=${TERMUX_PKG_VERSION}
-	export PYTORCH_BUILD_NUMBER=0
-	pip -v install --no-deps --no-build-isolation --prefix $TERMUX_PREFIX "$TERMUX_PKG_SRCDIR"
-	ln -sfr ${TERMUX_PYTHON_HOME}/site-packages/torch/lib/*.so ${TERMUX_PREFIX}/lib
+    export PYTORCH_BUILD_VERSION=${TERMUX_PKG_VERSION}
+    export PYTORCH_BUILD_NUMBER=0
+    pip -v install --no-deps --no-build-isolation --prefix $TERMUX_PREFIX "$TERMUX_PKG_SRCDIR"
+    ln -sfr ${TERMUX_PYTHON_HOME}/site-packages/torch/lib/*.so ${TERMUX_PREFIX}/lib
 }
